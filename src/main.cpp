a375ed553edcafdead669bf7194a271846888569
Sergio Zapata <56563008+sergiozap13@users.noreply.github.com>
2023-03-19 13:00:07 +0100

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
  * Curso 2022/2023
  */
 
-#include "Bigram.h"
-
-
 /* 
  * File:   main.cpp
  * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
@@ -32,7 +29,51 @@ using namespace std;
 #include <string>
 
 #include "Bigram.h"
+#include "BigramFreq.h"
+#include "ArrayBigramFreqFunctions.h"
+
 int main(int argc, char* argv[]) {
-    cout << "Hola";
+    
+    int n = 0;
+    
+    //cout << "Introduce the number of pairs BigramFreq to read: ";
+    cin >> n;
+    
+    const int TAM = n;
+    
+    //cout << "We proceed to read the bigrams: " << endl;
+    Bigram bi_aux;
+    std::string text = "";
+    int freq_aux = 0;
+    
+    BigramFreq array[TAM];
+    for(int i = 0; i < n; i++){
+        do{
+            //cout << "Introduce the bigram(" << i << ") text (a pair of characters): " << endl;
+            cin >> text;
+        } while(text.length() != 2);
+        
+        bi_aux.at(0) = text[0];
+        bi_aux.at(1) = text[1];
+        array[i].setBigram(bi_aux);
+        
+        //cout << "Introduce the frequency of the bigram (an integer number): " << endl;
+        cin >> freq_aux;
+        
+        array[i].setFrequency(freq_aux);
+    }
+    //cout << endl;
+    //cout << "Original Array: " << endl;
+    //printArrayBigramFreq(array, n);
+    
+    sortArrayBigramFreq(array, n);
+    
+    //cout << "Ordered Array: " << endl;
+//    printArrayBigramFreq(array, n);
+    
+    toUpperArrayBigramFreq(array, n);
+    
+    //cout << "Final Ordered Array: " << endl;
+    printArrayBigramFreq(array, n);
 }
 
